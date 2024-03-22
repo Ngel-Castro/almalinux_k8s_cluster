7847adc5c6da7201394bab2e2deeb013ef52b7fa
Angel Castro <castrobasurto.angel@gmail.com>
2024-03-22 11:12:31 -0400

    fixing merging conflicts

--- terraform/clusters/proxmox/outputs.tf
+++ terraform/clusters/proxmox/outputs.tf
@@ -1,15 +1,7 @@
 output "controllers_ips" {
-<<<<<<< HEAD
   value = tomap({"controllers" = local.controller_ips})
 }
 
 output "workers_ips" {
   value = tomap({"workers" = local.worker_ips})
-=======
-  value = local.controller_ips
-}
-
-output "workers_ips" {
-  value = local.worker_ips
->>>>>>> main
 }
